128d512054147c3ca427396c06a1b500faa7280f
trempoling69 <romain.mourieras@gmail.com>
2023-10-06 15:29:37 +0200

    last

--- src/unit-test/transfomUser.js
+++ src/unit-test/transfomUser.js
@@ -5,15 +5,22 @@ const transformUsers = (users) => {
   if (users.length > 8) {
     throw new Error('Too many users provided.');
   }
- return users.map((user) => {
+  return users.map((user) => {
     if (typeof user === 'string') {
       throw new Error('Users must be provided.');
     }
     const expectedUserObject = { id: '', name: '', age: '' };
+    if (typeof user.id !== 'number' || typeof user.body.userAge !== 'number') {
+      throw new Error('Id and age must be numbers');
+    }
+    if (typeof user.body.userName !== 'string') {
+      throw new Error('userName must be a string');
+    }
     expectedUserObject.id = user.id;
     expectedUserObject.name = user.body.userName;
     expectedUserObject.age = user.body.userAge;
     return expectedUserObject;
   });
 };
+
 module.exports = { transformUsers };
